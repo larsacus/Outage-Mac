053b054995295ab8e47d837e70162298b0daa31a
Lars Anderson <ishka912@gmail.com>
2013-01-01 19:25:07 -0600

    Initial Commit

---
+++ TimeOnBattery/TOLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TOLAppDelegate.h
+//  TimeOnBattery
+//
+//  Created by Lars Anderson on 1/1/13.
+//  Copyright (c) 2013 Lars Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TOLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TimeOnBattery/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TimeOnBattery
+//
+//  Created by Lars Anderson on 1/1/13.
+//  Copyright (c) 2013 Lars Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
